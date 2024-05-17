253a8c06544ce5fd3677efedf0417e08c8326876
Muhammad Kashan <112327092+Kashan724@users.noreply.github.com>
2024-05-17 21:55:29 +0500

    commited

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to CICD PIPELINE USING DOCKER,AWS AND GITHUB ACTIONS APP
+          Welcome to CICD PIPELINE USING DOCKER,AWS AND GITHUB ACTIONS APP DEPLOY
         </p>
         <a
           className="App-link"
